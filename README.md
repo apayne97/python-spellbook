5275f6e02ab2a5c589399a36db5c36ccdb21f5a1
Alexander Matthew Payne <alexpayne1997@gmail.com>
2025-03-14 12:59:56 -0400

    add environment

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python-spellbook
 A repository for testing and demonstrating various python functionalities.
+
+Mostly just a public, organized, gist repo.
--- conda_shenanigans/useful_commands.sh
+++ conda_shenanigans/useful_commands.sh
@@ -1,4 +1,4 @@
 # first of all, use mamba instead of conda
 
 # how to create an environment yml that is reasonably portable
-mamba env export --from-history>environment.yml
+mamba env export --from-history > python_spellbook.yml
